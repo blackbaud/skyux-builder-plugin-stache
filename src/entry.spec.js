d7044d2f64e1f497d0485e2fa04c2a673ab77cec
bdjones2410 <brandon.jones@blackbaud.com>
2017-08-10 10:42:55 -0400

    order test now makes sense

--- src/entry.spec.js
+++ src/entry.spec.js
@@ -14,52 +14,74 @@ describe('Entry Plugin', () => {
   it('should pass the content through all plugins', () => {
     let _content;
     let _resourcePath;
-    let _skyAppConfig;
+    let _skyPagesConfig;
     const mockPlugin = {
-      preload(content, resourcePath, skyAppConfig) {
+      preload(content, resourcePath, skyPagesConfig) {
         _content = content;
         _resourcePath = resourcePath;
-        _skyAppConfig = skyAppConfig;
+        _skyPagesConfig = skyPagesConfig;
         return content;
       }
     };
 
-    mock('./config', mockPlugin);
+    mock('./config.js', mockPlugin);
     mock('./include', mockPlugin);
-    mock('./code-block', mockPlugin);
-    mock('./json-data', mockPlugin);
-    mock('./route-metadata', mockPlugin);
-    mock('./template-reference-variable', mockPlugin);
+    mock('./code-block.js', mockPlugin);
+    mock('./json-data.js', mockPlugin);
+    mock('./route-metadata.js', mockPlugin);
+    mock('./template-reference-variable.js', mockPlugin);
 
     const plugin = new StacheEntryPlugin();
     const content = new Buffer('Content');
     const resourcePath = 'foo.html';
-    const skyAppConfig = {};
-    plugin.preload(content, resourcePath, skyAppConfig);
+    const skyPagesConfig = {};
+    plugin.preload(content, resourcePath, skyPagesConfig);
     expect(content.toString()).toEqual(_content.toString());
     expect(resourcePath).toEqual(_resourcePath);
-    expect(skyAppConfig.toString()).toEqual(_skyAppConfig.toString());
+    expect(skyPagesConfig.toString()).toEqual(_skyPagesConfig.toString());
   });
 
   it('should call the plugins in the expected order', () => {
     let callOrder = [];
-    const mockPlugin = {
-      preload(callNumber) {
-        callNumber++;
-        callOrder.push(callNumber);
-        return callNumber;
+    mock('./config.js', {
+      preload() {
+        callOrder.push(1);
       }
-    };
+    });
 
-    mock('./config', mockPlugin);
-    mock('./json-data', mockPlugin);
-    mock('./route-metadata', mockPlugin);
-    mock('./include', mockPlugin);
-    mock('./code-block', mockPlugin);
-    mock('./template-reference-variable', mockPlugin);
+    mock('./json-data.js', {
+      preload() {
+        callOrder.push(4);
+      }
+    });
+
+    mock('./route-metadata.js', {
+      preload() {
+        callOrder.push(5);
+      }
+    });
+
+    mock('./include', {
+      preload() {
+        callOrder.push(2);
+      }
+    });
+
+    mock('./code-block.js', {
+      preload() {
+        callOrder.push(3);
+      }
+    });
+
+    mock('./template-reference-variable.js', {
+      preload() {
+        callOrder.push(6);
+      }
+    });
 
     const plugin = new StacheEntryPlugin();
-    plugin.preload(0, '', {});
+    const content = new Buffer('Content');
+    plugin.preload(content, 'foo.html', {});
     expect(callOrder).toEqual([1, 2, 3, 4, 5, 6]);
   })
 });
