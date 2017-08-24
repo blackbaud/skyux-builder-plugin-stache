b68171ff327532a09e0b7fd15c93d358edb46c7e
Bobby Earl <bobby.earl@blackbaud.com>
2017-08-24 11:24:52 -0400

    Support nested includes (#17)

--- src/include.js
+++ src/include.js
@@ -3,37 +3,41 @@ const path = require('path');
 const cheerio = require('cheerio');
 const shared = require('./shared');
 
+const root = shared.resolveAssetsPath('includes');
+
+const replace = (content) => {
+
+  const $html = cheerio.load(content, shared.cheerioConfig);
+  const $includes = $html('stache-include');
+
+  if (!$includes.length) {
+    return content;
+  }
+
+  $includes.each((i, elem) => {
+    const $elem = $html(elem);
+    const fileName = $elem.attr('fileName');
+    const filePath = path.join(root, fileName);
+
+    // Recursively calling replace method to handle possible nested includes.
+    // Always calling replace since it'll short-circuit in the first if statement above.
+    try {
+      $elem.text(replace(fs.readFileSync(filePath)));
+    } catch (error) {
+      throw new shared.StachePluginError(error.message);
+    }
+
+  });
+
+  return $html.html().toString();
+}
+
 const preload = (content, resourcePath) => {
   if (!resourcePath.match(/\.html$/)) {
     return content;
   }
 
-  const $ = cheerio.load(content, shared.cheerioConfig);
-  const includes = $('stache-include');
-
-  if (!includes.length) {
-    return content;
-  }
-
-  const root = shared.resolveAssetsPath('includes');
-
-  includes.each((i, elem) => {
-    const $elem = $(elem);
-    const fileName = $elem.attr('fileName');
-    const filePath = path.join(root, fileName);
-
-    let contents;
-
-    try {
-      contents = fs.readFileSync(filePath);
-    } catch (error) {
-      throw new shared.StachePluginError(error.message);
-    }
-
-    $elem.text(contents);
-  });
-
-  return $.html().toString();
+  return replace(content);
 };
 
 module.exports = { preload };
