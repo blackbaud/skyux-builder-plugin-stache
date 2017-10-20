7777c0b13595a1258e4a36ae4923fa5ec55e8c20
Brandon Jones <brandon.jones@blackbaud.com>
2017-10-20 10:42:49 -0400

    Bugfix flag code (#25)
    * added flag for escaping chars
    
    * renamed flag
    
    * updated selector

--- src/code.spec.js
+++ src/code.spec.js
@@ -19,9 +19,9 @@ describe('Code Plugin', () => {
     expect(result.toString()).toEqual(content.toString());
   });
 
-  it('should convert the inner HTML of all <stache-code> to HTML entities.', () => {
+  it('should convert the inner HTML of all <stache-code> to HTML entities if the "escapeCharacters" flag is true.', () => {
     const content = new Buffer(`
-      <stache-code>
+      <stache-code escapeCharacters="true">
         <p>My content</p>
         {{ myVar }}
         $(document).ready();
@@ -31,5 +31,24 @@ describe('Code Plugin', () => {
     const result = plugin.preload(content, path);
     expect(result.toString()).toContain('&lt;p>My content&lt;/p>');
     expect(result.toString()).toContain('{{ \'{\' }}{{ \'{\' }} myVar }}');
+  });
+
+  it('should not convert the inner HTML of all <stache-code> to HTML entities if "escapeCharacters" attribute is not true', () => {
+    const content = new Buffer(`
+      <stache-code>
+        <p>My content</p>
+        {{ myVar }}
+        $(document).ready();
+      </stache-code>
+      <stache-code escapeCharacters="fooBar">
+        <p>My content</p>
+        {{ myVar }}
+        $(document).ready();
+      </stache-code>
+    `);
+    const path = 'foo.html';
+    const result = plugin.preload(content, path);
+    expect(result.toString()).not.toContain('&lt;p>My content&lt;/p>');
+    expect(result.toString()).not.toContain('{{ \'{\' }}{{ \'{\' }} myVar }}');
   });
 });
